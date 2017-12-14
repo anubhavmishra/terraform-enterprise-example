3fb99d4b13862d83dcbc78ee5e235eb0dd1f7ade
Anubhav Mishra <anubhavmishra@me.com>
2017-12-13 16:46:42 -0800

    updates

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 terraform {
   backend "atlas" {
     name         = "anubhavmishra-tfe/terraform-enterprise-example"
+    access_token = ${var.access_token}
   }
 }
 
 resource "null_resource" "example" {
+}
+
+variable "access_token" {
+  default = ""
 }
