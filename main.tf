e776f517343e0a8310743b52b0e719601375a646
Anubhav Mishra <anubhavmishra@me.com>
2017-12-13 16:54:23 -0800

    remove interpolation from the backend config

--- main.tf
+++ main.tf
@@ -1,13 +1,8 @@
 terraform {
   backend "atlas" {
     name         = "anubhavmishra-tfe/terraform-enterprise-example"
-    access_token = "${var.access_token}"
   }
 }
 
 resource "null_resource" "example" {
-}
-
-variable "access_token" {
-  default = ""
 }
